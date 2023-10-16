603dc1b83c48640cfb0bd3ca716424b6378df837
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-16 13:50:34 -0700

    printf_project commit 55

---
+++ _get_size.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_size - Calculates the size to cast the argument
+ * @format: The string
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+	int a = *i + 1;
+	int size = 0;
+
+	if (format[a] == 'l')
+		size = S_LONG;
+	else if (format[a] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = a - 1;
+	else
+		*i = a;
+
+	return (size);
+}
+
